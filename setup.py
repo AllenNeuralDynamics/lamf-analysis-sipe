e9c083c631a843d3e65c7fbc2b4dd6371a521c41
Christopher <chrism@alleninstitute.org>
2025-04-21 09:06:14 -0700

    switch to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
-from setuptools import setup, find_packages
+# setup.py (optional)
+# Not strictly needed unless you have custom logic
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+from setuptools import setup
 
-setup(
-    name='lamf_analysis',
-    version='0.1.5',
-    url='https://github.com/AllenNeuralDynamics/ophys-mfish-dev',
-    author='Matthew J. Davis',
-    author_email='mattjdavis@gmail.com',
-    description='TODO',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),
-    install_requires=requirements,
-)
+setup()
